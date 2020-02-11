6b7c79e3b744a9b8c1c8fe75c5a961287ced6074
umair9631 <umair9631@gmail.com>
2020-02-11 16:58:53 +0500

    skills json response.

--- app/controllers/api/v1/skills_controller.rb
+++ app/controllers/api/v1/skills_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::SkillsController < Api::V1::ApiController
 
     def index
       @skills = Skill.all.map { |s| s.parent_id == 0  }
+      return render json: { success: true, msg: 'Skills List.', data: @skills} , status: 200
     end
 
 end
